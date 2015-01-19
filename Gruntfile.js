89505d4f6decbc22767aa9fa03364b1904bdfc23
dkrainas87@gmail.com <dkrainas87@gmail.com>
2015-01-19 16:21:31 -0500

    smooth out build process

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,14 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 module.exports = function(grunt) {
-    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         
         jshint: {
             all: {
-                src: ['index.js', 'src/critr.js'],
+                src: ['index.js', 'src/<%= pkg.name %>.js'],
                 options: {
-                    jshintrc: true,
                     globalstrict: true,
                     globals: {
                         require: false,
@@ -24,10 +24,36 @@ module.exports = function(grunt) {
                 reporter: 'spec'
             },
             src: ['test/mocha/*.js']
+        },
+        uglify: {
+            options: {
+                preserveComments: 'some'
+            },
+            dist: {
+                files: {
+                    'dist/<%= pkg.name %>.min.js': 'dist/<%= pkg.name %>.js'
+                }
+            }
+        },
+        concat: {
+            options: {
+                banner: '/*!\n * <%= pkg.name %> <%= pkg.version %>. (<%= pkg.homepage %>)\n * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>\n */\n\n'
+            },
+            dist: {
+                src: 'src/<%= pkg.name %>.js',
+                dest: 'dist/<%= pkg.name %>.js'
+            }
         }
     });
 
-    grunt.registerTask('default', ['jshint', 'test']);
+    if (process.env.NODE_ENV === 'development') {
+        grunt.loadNpmTasks('grunt-mocha-test');
+        grunt.loadNpmTasks('grunt-contrib-jshint');
+        grunt.loadNpmTasks('grunt-contrib-uglify');
+        grunt.loadNpmTasks('grunt-contrib-concat');
 
-    grunt.registerTask('test', ['mochaTest']);
+        grunt.registerTask('test', ['mochaTest']);
+        grunt.registerTask('default', ['jshint', 'test']);
+        grunt.registerTask('build', ['test', 'concat:dist', 'uglify:dist']);
+    }
 };
