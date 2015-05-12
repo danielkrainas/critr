3b202643512d4e181b06cf0c0d8b7441f2ee3bad
dkrainas87@gmail.com <dkrainas87@gmail.com>
2015-05-11 23:52:06 -0400

    add coverage support

--- Gruntfile.js
+++ Gruntfile.js
@@ -45,6 +45,20 @@ module.exports = function(grunt) {
                 src: 'src/<%= pkg.name %>.js',
                 dest: 'dist/<%= pkg.name %>.js'
             }
+        },
+        mocha_istanbul: {
+            coverage: {
+                src: 'test/mocha',
+                options: {
+                    coverageFolder: '.coverage'
+                }
+            }
+        },
+        codeclimate: {
+            options: {
+                file: '.coverage/lcov.info',
+                token: process.env.CODECLIMATE_TOKEN
+            }
         }
     });
 
@@ -53,9 +67,12 @@ module.exports = function(grunt) {
         grunt.loadNpmTasks('grunt-contrib-jshint');
         grunt.loadNpmTasks('grunt-contrib-uglify');
         grunt.loadNpmTasks('grunt-contrib-concat');
+        grunt.loadNpmTasks('grunt-mocha-istanbul');
 
         grunt.registerTask('test', ['mochaTest']);
         grunt.registerTask('default', ['jshint', 'test']);
+        grunt.registerTask('coverage', ['mocha_istanbul']);
+        grunt.registerTask('coverage:report', ['mocha_istanbul', 'codeclimate'])
         grunt.registerTask('build', ['test', 'concat:dist', 'uglify:dist']);
     }
 };
