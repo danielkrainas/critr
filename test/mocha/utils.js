a33a056a555f6c2c2c9704ed076f7ef24da1ae36
dkrainas87@gmail.com <dkrainas87@gmail.com>
2015-05-14 01:21:22 -0400

    more tests

---
+++ test/mocha/utils.js
@@ -0,0 +1,21 @@
+var chai = require('chai');
+var expect = chai.expect;
+var utils = require('../../src/utils');
+
+describe('Utils:', function () {
+    var data;
+
+    describe('deepCompare', function () {
+        beforeEach(function () {
+            data = { name: 'bob', home: { windows: 2 }};
+        });
+
+        it('should return true if objects are deep equal', function () {
+            expect(utils.deepCompare({ name: 'bob', home: { windows: 2 }}, data)).to.be.true;
+        });
+
+        it('should return false if objects are not deeply equal', function () {
+            expect(utils.deepCompare({ name: 'bob', home: { windows: 3 }}, data)).to.be.false;
+        });
+    });
+});
